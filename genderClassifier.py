428c9a17ea311026278183f933b190f41c49bd58
Sohail Rashid <sohail.ra5@gmail.com>
2017-07-14 17:38:31 -0400

    update

--- genderClassifier.py
+++ genderClassifier.py
@@ -7,6 +7,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 import numpy as np
 
+print("Hello Sohail")
 # Data and labels
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37],
      [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40],
---
+++ ticTacToe_NN.js
@@ -0,0 +1,7 @@
+function setup() {
+	createCanvs(600, 600);
+}
+
+function draw() {
+	background(51);
+}
